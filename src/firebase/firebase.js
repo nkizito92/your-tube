8d1826b462529e851564cc9f18cfff79c7a1d89a
Kizito Njoku <nkizito57@gmail.com>
2022-09-29 17:59:09 -0500

    firebase added

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAU_uVjqL-U8Ry0e_ADbtvxfW40q5ESzxg",
+    authDomain: "yourtube-eda83.firebaseapp.com",
+    projectId: "yourtube-eda83",
+    storageBucket: "yourtube-eda83.appspot.com",
+    messagingSenderId: "354372168609",
+    appId: "1:354372168609:web:5f4f32b2b714734517f9e0",
+    measurementId: "G-1YNL55Z8RE"
+  };
