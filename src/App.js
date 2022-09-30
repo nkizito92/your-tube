5629cebaccdae8e4509f9e444d8536b7e4e888be
Kizito Njoku <nkizito57@gmail.com>
2022-09-29 20:56:12 -0500

    added a  main tag for the body of yourTUbe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import Header from './components/Header';
+import Home from './components/Home';
+import  {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar/Sidebar';
+import RecommendedVideos from './components/RecommendedVideos/RecommendedVideos';
+import SearchPage from './components/SearchPage/SearchPage';
 function App() {
+  // Bem class naming convention
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Header/>
+        <div className='app__page'>
+          <Sidebar />
+            <main>
+              <Routes> 
+                <Route exact path="/" element={<RecommendedVideos />} />
+                <Route path="/search/:searched" element={<SearchPage />}/>
+              </Routes>
+            </main>
+        </div>
+      </Router>
     </div>
   );
 }
