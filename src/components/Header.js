6d125ad1b5fd5016b1c317bca95d63da3bc14f46
Kizito Njoku <nkizito57@gmail.com>
2022-10-03 21:59:33 -0500

    set the img tag up with the yourtube logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 import VideoCall from '@mui/icons-material/VideoCall';
 import SearchIcon from '@mui/icons-material/Search';                   
 import AppsIcon from '@mui/icons-material/Apps';                   
 import NotificationIcon from '@mui/icons-material/Notifications';  
 import Avatar from '@mui/material/Avatar'
+import {Link} from "react-router-dom"
 import "./header.scss"
 const Header = () => {
-  
-  let coloring = {
-    background: "#cd9d00",
-    padding: "5px",
-    color: 'white',
-    borderRadius: "10px"
-  }
-
+   const [inputSearch, setInputSearch] = useState("")
   return (
     <div className='header'>
       <div className='header__left'>
         <MenuIcon className="header__icon"/>
-        {/* image */}
-        <span className="header__logo">Your<span style={coloring}>Tube</span></span>
-
+        <Link to="/">
+         <img 
+          className='header__logo'
+          src={require("../assets/images/yourTubeIcon.png")} alt="YourTube" />
+        </Link>
       </div>
       <div className='header__input'>
-        <input type="text" placeholder='Search'/>
-        <SearchIcon className="header__inputBtn"/> 
+        <input type="text" onChange={(e) => setInputSearch(e.target.value)} value={inputSearch} placeholder='Search'/>
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header__inputBtn"/> 
+        </Link>
         
       </div>
       <div className='header__icons'>
@@ -35,7 +33,6 @@ const Header = () => {
         <NotificationIcon className="header__icon" />
         <Avatar src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F3%2F3a%2FCat03.jpg%2F1200px-Cat03.jpg&imgrefurl=https%3A%2F%2Fen.wiktionary.org%2Fwiki%2Fcat&tbnid=hMjxy8pUhhc4QM&vet=12ahUKEwjI8K_Y9qn6AhXKB1MKHWzXBvkQMygAegUIARDcAQ..i&docid=3aBlXpmFZqFG2M&w=1200&h=1199&q=cat&ved=2ahUKEwjI8K_Y9qn6AhXKB1MKHWzXBvkQMygAegUIARDcAQ"/>
       </div>
-
     </div>
   )
 }
