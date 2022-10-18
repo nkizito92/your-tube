28c40e1313d81cd5b50b2ccb76d00d68cb9636f0
Kizito Njoku <nkizito57@gmail.com>
2022-10-17 22:12:33 -0500

    thumbnail image and Avatar image has been added

--- src/components/Videos/VideoCard.js
+++ src/components/Videos/VideoCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Avatar from '@mui/material/Avatar'
-import './videoCard.scss'
+import './scss/videoCard.scss'
 const VideoCard = ({image, title, channel, timestamp, views, channelImage}) => {
   return (
     <div className='videoCard'>
-       <img className="videoCard__thumbnail" src={image}/>
+       <img className="videoCard__thumbnail" src={require('../assets/images/profile.png')}/>
       <div className="videoCard__info">
-        <Avatar className="videoCard__avatar" src={channelImage} alt={channel} />
+        <Avatar className="videoCard__avatar" src={require('../assets/images/profile.png')}/>
         <div className='videoCard__text'>
           <h4>{title}Titles are Awesome to See</h4>
-          <p>{channel}chann</p>
-          <p>{views}89 * {timestamp}06/55/1922</p>
+          <p>{channel}</p>
+          <p>{views}89 • {timestamp}06/55/1922</p>
         </div>
       </div>
     </div>
