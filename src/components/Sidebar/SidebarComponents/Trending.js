0181f3a16bc905e9d6715dfe403574284304102e
Kizito Njoku <nkizito57@gmail.com>
2023-03-26 22:46:05 -0500

    built a trending component for the sidebar

---
+++ src/components/Sidebar/SidebarComponents/Trending.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Trending = () => {
+    return (
+        <div>
+            Trending stuff
+            <h1>Hello there</h1>
+        </div>
+    )
+}
+export default Trending
