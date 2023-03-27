3996aae4e3d90051d0185d2382d4e056b75f32e3
Kizito Njoku <nkizito57@gmail.com>
2023-03-26 22:47:49 -0500

    sidebarRow links are directing you to the routes when selected

--- src/components/Sidebar/SidebarRow.js
+++ src/components/Sidebar/SidebarRow.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./sidebarRow.scss"
 const SidebarRow = ({selected, Icon, title }) => {
+  let links = ""
+  title === 'Home' ? links = '/' : links = title
+  if(title === 'Home') links ="/"
   return (
     <div className={`sidebarRow ${selected && "selected"}`}> 
         <Icon className="sidebarRow__icon" />
-        <h2 className='sidebarRow__title'>{title}</h2>
+        <Link to={`${links.toLowerCase()}`}>   
+          <h2 className='sidebarRow__title'>{title}</h2>
+        </Link>
     </div>
   )
 }
