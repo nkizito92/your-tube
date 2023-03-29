145fea8c77085a552b34c66e1f00b6b8b6fa8b30
Kizito Njoku <nkizito57@gmail.com>
2023-03-28 20:57:58 -0500

    more routes have been added for the sideBar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Header from './components/Header';
 import  {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Sidebar from './components/Sidebar/Sidebar';
 import RecommendedVideos from './components/RecommendedVideos/RecommendedVideos';
+import Trending from "./components/Sidebar/SidebarComponents/Trending"
+import History from "./components/Sidebar/SidebarComponents/History"
+import Subscription from "./components/Sidebar/SidebarComponents/Subscription"
+import Library from "./components/Sidebar/SidebarComponents/Library"
+import Yourvideos from "./components/Sidebar/SidebarComponents/Yourvideos"
 import SearchPage from './components/SearchPage/SearchPage';
 function App() {
   // Bem class naming convention
@@ -15,6 +20,11 @@ function App() {
             <main>
               <Routes> 
                 <Route exact path="/" element={<RecommendedVideos />} />
+                <Route exact path="/trending" element={<Trending />} />
+                <Route exact path="/history" element={<History />} />
+                <Route exact path="/subscription" element={<Subscription />} />
+                <Route exact path="/yourvideos" element={<Yourvideos />} />
+                <Route exact path="/library" element={<Library />} />
                 <Route path="/search/:searched" element={<SearchPage />}/>
               </Routes>
             </main>
