6b9b4c90a55f1458e21a3502a29f93d068568123
Kizito Njoku <nkizito57@gmail.com>
2023-03-30 20:13:31 -0500

    created a demoApi to use for testing

---
+++ src/demoApi/demoApi.js
@@ -0,0 +1,28 @@
+export const demoApi = 
+[
+  { 
+    title: 'Favorite Car', 
+    image: "Image",
+    channel: "Giggby", 
+    timestamp: "223:84:54",
+    views: "2269", 
+    channelImage: 'chnImage'
+  },
+  { 
+    title: 'Coding tutorials', 
+    image: "Image2",
+    channel: "Zeepss", 
+    timestamp: "13:84:54",
+    views: "3469", 
+    channelImage: 'chnImage2'
+  }, 
+  { 
+    title: 'Interview Prep', 
+    image: "Image3",
+    channel: "Suzs", 
+    timestamp: "6:24:54",
+    views: "8946", 
+    channelImage: 'chnImage3'
+  }
+]
+
